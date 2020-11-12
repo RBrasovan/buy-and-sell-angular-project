2f9d44724e157aa809809de6a72dc197b763d5c7
Rachael Brasovan <rachaelb@rachaelcbookpro.intranet.unthsc.edu>
2020-11-12 16:47:43 -0600

    Update before leaving for the day

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListingsPgComponent } from './listings-pg/listings-pg.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'listings', component : ListingsPgComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
