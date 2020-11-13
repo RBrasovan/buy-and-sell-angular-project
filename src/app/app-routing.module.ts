b9481e6f65b23b845bea26706eb24ee83c39b729
Rachael Brasovan <rachaelb@dynamic-10-8-14-194.dynamic.unt.edu>
2020-11-13 08:32:36 -0600

    added more pages/components and styling and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 import { ListingsPgComponent } from './listings-pg/listings-pg.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
 const routes: Routes = [
-  {path : 'listings', component : ListingsPgComponent}
+  {path : '', redirectTo : '/listings', pathMatch : 'full'},  //so this is a redirect so if you are on the "home" page, it will redirect you to the /listings page which we made a route for below.
+  {path : 'listings', component : ListingsPgComponent, pathMatch : 'full'},
+  {path : 'listings/:id', component : ListingDetailPageComponent}, //:id is a URL parameter and is also why pathMatch : 'full' is used on the row above.
+  {path : 'contct/:id', component : ContactPageComponent},
+  {path : 'edit-listing/:id', component : EditListingPageComponent},
+  {path : 'my-listings', component : MyListingsPageComponent},
+  {path : 'new-listing', component : NewListingPageComponent}
 ];
 
 @NgModule({
