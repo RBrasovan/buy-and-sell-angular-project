e49c28cffe5d68f3175700695a81f001c3bf7287
Rachael Brasovan <rachaelb@dynamic-10-8-14-194.dynamic.unt.edu>
2020-11-13 10:11:02 -0600

    update

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingDetailPageComponent implements OnInit {
 
-  constructor() { }
+  //So now that we have that we want to use it to get the correct Listing data based off of it.
+  listing : Listing;  //variable listing of object type Listing
+
+  //activated route allows us to access he URL parameter value
+  constructor(private route : ActivatedRoute, ) {
+
+  }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id'); //here it is actually getting the parameter value from the URL.
+    this.listing = fakeListings.find(listing => listing.id === id); 
   }
 
 }
--- src/app/listings-pg/listings-pg.component.ts
+++ src/app/listings-pg/listings-pg.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listings-pg',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings-pg.component.css']
 })
 export class ListingsPgComponent implements OnInit {
+  listings : Listing[] = [];  //how you make an array. In this case an array called listings of type Listing (class found in ../types). Right now it is assigned an empty array (dynamic by default?).
 
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeListings; //the proper place to put an assignment like this. Will be used for the server later on.
   }
 
 }
