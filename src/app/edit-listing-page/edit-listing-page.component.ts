b2138b6f8d837da41ba7f58f6acc3be06dc8d26c
Rachael Brasovan <rachaelb@dynamic-10-1-15-137.dynamic.unt.edu>
2020-11-16 08:24:43 -0600

    updates to edit and add listing components

--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
 
 @Component({
   selector: 'app-edit-listing-page',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditListingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router,) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() : void {
+    alert("Changing things to listing...");
+    this.router.navigateByUrl('/my-listings');
   }
 
 }
